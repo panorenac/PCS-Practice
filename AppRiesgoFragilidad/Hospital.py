f94fd6ab6e19b466848364cb84195a4010a1089c
panorenac <49770509+panorenac@users.noreply.github.com>
2024-11-17 10:31:23 -0500

    Add files via upload

---
+++ AppRiesgoFragilidad/Hospital.py
@@ -0,0 +1,18 @@
+class HOSPITAL:
+  def __init__ (self, nit, nombre, direccion):
+        self.NIT = nit 
+        self.NOMBRE = nombre 
+        self.DIRECCION = direccion
+      
+  LISTA_HOSPITALES = []
+
+h1 = HOSPITAL(890901826-2, "Pablo Tobón Uribe", "Diagonal 80 #76-08, Robledo")
+h2 = HOSPITAL(890906347-9, "Manuel Uribe Ángel", "Diagonal 31 36ª sur 80, Envigado")
+LISTA_HOSPITALES = [h1, h2]
+
+def imprimirHospital():
+  for hospital in LISTA_HOSPITALES:
+    print ("Nit: ", hospital.NIT)
+    print ("Nombre: ", hospital.NOMBRE)
+    print ("Dirección: ", hospital.DIRECCION)
+    print ("")
---
+++ AppRiesgoFragilidad/RitmoCardiaco.py
@@ -0,0 +1,10 @@
+import random
+
+
+class RITMOCARDIACO:
+  def __init__ (self, tiempo,valor,riesgofragilidad):
+    self.TIEMPOR= tiempo
+    self.VALOR = valor
+    self.RIESGOFRAGILIDAD =riesgofragilidad
+
+
